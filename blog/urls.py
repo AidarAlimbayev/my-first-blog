3a34fce44c22cafd71569eadf932fe44f5b1e5dd
AidarAlimbayev <transcription@rambler.ru>
2020-01-01 00:50:39 +0600

    Finished course. djangogirlstutorial

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,17 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
+
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('', views.post_list, name='post_list'),
+#     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+# ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,70 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.utils import timezone
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Post
+from .forms import PostForm
+
+
+# lte : less than equal     <=
+# lt : less than            <
+# gte : greater than equal  >=
+# gt : greater than         >
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+    
+
+def post_detail(request, pk):
+#   try:
+#       post = Post.objects.get(pk=pk)  # Post.DoesNotExist
+#   except Post.DoesNotExist:
+#       raise Http404  # django.http.Http404
+
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+@login_required(login_url='admin:login')
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail', post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+@login_required(login_url='admin:login')
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+# from django.shortcuts import render
+# from django.utils import timezone
+# from .models import Post
+# from django.shortcuts import render, get_object_or_404
+
+# def post_list(request):
+#     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+#     return render(request, 'blog/post_list.html', {'posts': posts})
+#     Post.objects.get(pk=pk)
+
+# def post_detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     return render(request, 'blog/post_detail.html', {'post': post})
